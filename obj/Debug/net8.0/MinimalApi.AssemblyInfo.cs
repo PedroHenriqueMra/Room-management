144fd88a9f6359857f2a9fb74fb236a81147ae60
PedroHenriqueMra <pedrohenriquemraa@gmail.com>
2024-09-24 15:58:59 -0300

    test b-master

--- obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinimalApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693727e0c3c2a7d2accef54eb898acd7ea588a8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
