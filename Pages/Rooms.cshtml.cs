8762620f0ae81bc862622314516158f9755b994f
PedroHenriqueMra <pedrohenriquemraa@gmail.com>
2024-09-25 21:04:31 -0300

    add the room page

--- Pages/Rooms.cshtml.cs
+++ Pages/Rooms.cshtml.cs
@@ -1,18 +1,31 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ModelTables;
 
 [Authorize]
 public class RoomModel : PageModel
 {
     public ILogger<RoomModel> _logger;
+    public DbContextModel _context;
 
-    public RoomModel(ILogger<RoomModel> logger)
+    public RoomModel(ILogger<RoomModel> logger, DbContextModel context)
     {
+        _context = context;
         _logger = logger;
     }
 
-    public void OnGet()
+    public List<Room> Rooms { get; set; } = new List<Room>();
+
+    public async Task OnGetAsync()
     {
-        
+        var rooms = await _context.Room.ToListAsync();
+        if (rooms.Count != 0)
+        {
+            foreach(var r in rooms)
+            {
+                Rooms.Add(r);
+            }
+        }
     }
 }
